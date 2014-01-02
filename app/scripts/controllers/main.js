40c9614633a3c48f64105c9647307b7b685d8c25
Andres Sainz de Aja <andres.sainz@globant.com>
2014-01-01 21:16:37 -0300

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('navigationManagerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
